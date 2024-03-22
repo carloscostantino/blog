b5b172b03ff0779f6d26a37c3ae061365438211c
carloscostantino <carloscostantino@gmail.com>
2024-03-22 12:55:48 -0300

    Generar registros de prueba con factory

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->unique()->realText(55);
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'introduction' => $this->faker->realText(255),
+
+        ];
+    }
+}
